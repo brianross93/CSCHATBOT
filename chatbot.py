6d8dd928c589dc960308ac0e49a0842115aa6d6f
Brian Ross <brian.ross@students.makeschool.com>
2020-09-22 04:41:58 -0400

    Added some functionality

---
+++ chatbot.py
@@ -0,0 +1,30 @@
+from random import choice 
+
+
+
+
+def chat_init(userInput):
+    pickup_line = ["What's up buttercup?", "What's cookin good lookin?", "Holy guacamole!"]
+
+    user_reply_positive = ["Let's rock and roll!", "Saddle up and ride!", ""]
+    user_reply_negative = 
+
+    if user_response == "Wow what a hunk":
+        for i in range(10):
+            print("Wow!")
+    
+
+    
+
+
+
+user_Input = ""
+
+while True:
+  user_response = input("Woah momma!"))
+  
+  if user_response == 'go away':
+    break
+
+pickup_response = chat_init(user_response)
+print(pickup_response)
