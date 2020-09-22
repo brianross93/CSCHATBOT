4ef0da81e3bdc0bff1536444a2f6a409d99ebd7c
Brian Ross <brian.ross@students.makeschool.com>
2020-09-22 05:24:36 -0400

    Johnny Bravo Bot Activated

--- chatbot.py
+++ chatbot.py
@@ -3,28 +3,37 @@ from random import choice
 
 
 
-def chat_init(userInput):
-    pickup_line = ["What's up buttercup?", "What's cookin good lookin?", "Holy guacamole!"]
+def chat_init(user_response):
+    pickup_line = ["Let's rock and roll rockstar!", "Giddy up!", "What's up buttercup?",
+    "What's cookin good lookin?", "Holy guacamole!"]
 
-    user_reply_positive = ["Let's rock and roll!", "Saddle up and ride!", ""]
-    user_reply_negative = 
+    pickup_line_alternate = ["I say we make like a banana and split", 
+    "How about you and me go dance with destiny", "Let's make like a bee and buzz outta here"]
 
     if user_response == "Wow what a hunk":
-        for i in range(10):
-            print("Wow!")
-    
+        return "Say okay and we go away!"
+    elif user_response == "What?":
+        return choice(pickup_line_alternate)
+    elif user_response == "Stop":
+        return "Say go away and I make like hay and go away"
+    else:
+        return choice(pickup_line)
 
     
 
 
 
-user_Input = ""
+user_response = ""
 
 while True:
-  user_response = input("Woah momma!"))
+    user_response = input("Woah momma! ")
   
-  if user_response == 'go away':
-    break
+    if user_response == 'Go away':
+        break
+    if user_response == "Okay":
+        user_other_response = "Cmon hunny lets blow this joint"
+        print(user_other_response)
+        break
 
-pickup_response = chat_init(user_response)
-print(pickup_response)
+    pickup_response = chat_init(user_response)
+    print(pickup_response)
